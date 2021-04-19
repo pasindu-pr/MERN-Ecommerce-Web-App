e7ffcf89a6d6edbb0ba58b0f8510f623e76eaaf0
Your Name <you@example.com>
2021-04-19 19:03:43 +0530

    Stripe Error Fix

--- frontend/src/wrapper/PaymentScreenWrapComponent.js
+++ frontend/src/wrapper/PaymentScreenWrapComponent.js
@@ -3,7 +3,9 @@ import PaymentScreen from "../screens/PaymentScreen";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISH_KEY);
+const stripePromise = loadStripe(
+  "pk_test_51ISjvGKyAujpXbVztUCNyO2cCURe88SeftvB6sGnTtH8OJ8cp9B79Zz2QvZA0SJy8KpG6qjCDMQBpZuJUiDEZk6h00nkaDylIa"
+);
 
 const PaymentScreenWrapComponent = () => {
   return (

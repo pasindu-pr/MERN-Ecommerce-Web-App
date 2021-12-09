ce553573884f821621530583ed55b6e853f00ca0
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2021-12-09 21:38:51 +0530

    Update Nav bar

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import PersonRoundedIcon from "@material-ui/icons/PersonRounded";
-import ShoppingBasketRoundedIcon from "@material-ui/icons/ShoppingBasketRounded";
 import { IconButton } from "@material-ui/core";
 import DehazeIcon from "@material-ui/icons/Dehaze";
 import CloseIcon from "@material-ui/icons/Close";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
+import { Header, Divider, Label } from "semantic-ui-react";
 
 import mainlogo from "../images/logo/emporium-logo.png";
 import categories from "./categories";
@@ -44,54 +43,77 @@ const NavBar = () => {
           </Link>
         </Logo>
 
-        <Items>
-          <ul>
-            {categories && (
-              <>
-                {categories.map((category, index) => (
-                  <Link key={index} to={`/category/${category.name}/page/1`}>
-                    <li onClick={navBarHandler}>{category.name}</li>
-                  </Link>
-                ))}
-              </>
-            )}
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Items>
+            <ul>
+              <Divider horizontal>
+                <Header as="h2" className="heading">
+                  Categories
+                </Header>
+              </Divider>
+              {categories && (
+                <>
+                  {categories.map((category, index) => (
+                    <Link key={index} to={`/category/${category.name}/page/1`}>
+                      <li onClick={navBarHandler}>{category.name}</li>
+                    </Link>
+                  ))}
+                </>
+              )}
 
-            {loggedUser && loggedUser.accountType === "Administrator" && (
-              <li onClick={dashboardClickHandle}> Dashboard - Admin </li>
-            )}
-          </ul>
-        </Items>
+              {loggedUser && loggedUser.accountType === "Administrator" && (
+                <li onClick={dashboardClickHandle}> Dashboard - Admin </li>
+              )}
+            </ul>
+          </Items>
 
-        {Object.keys(loggedUser).length !== 0 ? (
-          <Link to="/my-account">
-            <UserDiv>
-              <PersonRoundedIcon style={{ fill: "#fb743e" }} />
-              <div style={{ marginLeft: "10px" }}> {loggedUser.name} </div>
-            </UserDiv>
+          {Object.keys(loggedUser).length !== 0 ? (
+            <Link to="/my-account">
+              <UserDiv>
+                <img
+                  className="user-icon"
+                  src="/images/user-profile-icon.png"
+                  alt="usericon"
+                />
+                <div style={{ marginLeft: "10px" }}> {loggedUser.name} </div>
+              </UserDiv>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <UserDiv>
+                <img
+                  className="user-icon"
+                  src="/images/user-profile-icon.png"
+                  alt="usericon"
+                />
+                <div style={{ marginLeft: "10px" }}> My Account </div>
+              </UserDiv>
+            </Link>
+          )}
+
+          <Link to="/mycart">
+            <CartDiv>
+              <div>
+                <img
+                  className="cart-icon"
+                  src="/images/shopping-bag.png"
+                  alt="cart-icon"
+                ></img>
+                <Label circular size="tiny" color="orange">
+                  {cartProducts.length === 0 ? 0 : `${cartProducts.length}`}
+                </Label>
+              </div>
+
+              <div style={{ marginLeft: "10px" }}>My Cart</div>
+            </CartDiv>
           </Link>
-        ) : (
-          <Link to="/login">
-            <UserDiv>
-              <PersonRoundedIcon style={{ fill: "#fb743e" }} />
-              <div style={{ marginLeft: "10px" }}> My Account </div>
-            </UserDiv>
-          </Link>
-        )}
 
-        <Link to="/mycart">
-          <CartDiv>
-            <ShoppingBasketRoundedIcon style={{ fill: "#fb743e" }} />
-            <div style={{ marginLeft: "10px" }}>
-              Cart {cartProducts.length === 0 ? "" : `(${cartProducts.length})`}
-            </div>
-          </CartDiv>
-        </Link>
-
-        <ScrollBarDiv>
-          <IconButton onClick={navBarHandler}>
-            {navBar ? <DehazeIcon /> : <CloseIcon />}
-          </IconButton>
-        </ScrollBarDiv>
+          <ScrollBarDiv>
+            <IconButton onClick={navBarHandler}>
+              {navBar ? <DehazeIcon /> : <CloseIcon />}
+            </IconButton>
+          </ScrollBarDiv>
+        </div>
       </MainContainer>
     </>
   );
@@ -100,8 +122,10 @@ const NavBar = () => {
 const MainContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-between;
   font-family: "Nunito", sans-serif;
+  height: 3.5rem;
+  margin: 0 1rem;
 
   .cartNumber {
     font-size: 10px;
@@ -113,23 +137,21 @@ const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
     position: absolute;
-    top: 0;
+    top: calc(3.5rem + 35px);
     left: 0;
     color: white;
-    background: grey;
-    height: 100vh;
+    background: white;
+    height: calc(100vh - (3.5rem + 35px));
     width: 25vw;
     z-index: 10;
     padding: 2rem;
-    border-top-right-radius: 2rem;
-    border-bottom-right-radius: 2rem;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
     overflow: hidden;
-    -webkit-box-shadow: 10px 0px 21px 4px rgba(201, 201, 201, 0.66);
-    -moz-box-shadow: 10px 0px 21px 4px rgba(201, 201, 201, 0.66);
-    box-shadow: 10px 0px 21px 4px rgba(201, 201, 201, 0.66);
     transition: 0.8s all;
     transform: ${(props) =>
       props.toggle ? "translate(-500px)" : " translate(0)"};
+    box-shadow: 5px 1px 0 rgba(0, 0, 0, 0.1);
 
     @media (max-width: 576px) {
       width: 60vw;
@@ -138,7 +160,7 @@ const MainContainer = styled.div`
     li {
       padding-top: 2rem;
       text-decoration: none;
-      color: white;
+      color: black;
     }
   }
 `;
@@ -175,6 +197,10 @@ const UserDiv = styled.div`
       display: none;
     }
   }
+
+  .user-icon {
+    width: 30px;
+  }
 `;
 
 const CartDiv = styled.div`
@@ -186,6 +212,14 @@ const CartDiv = styled.div`
       display: none;
     }
     margin: 0rem;
+
+    .cart-icon {
+      margin: 0 20px;
+    }
+  }
+
+  .cart-icon {
+    width: 20px;
   }
 `;
 
